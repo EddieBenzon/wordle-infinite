c1b6ab1ee1cc85dd7ae7694e129988eaebc3a1e5
Eddie Benzon <eddie.benzon91@gmail.com>
2022-08-16 12:18:56 +0200

    CSS adjustments

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { AppProvider } from "./Components/context";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AppProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </AppProvider>
 );
